741482071ae2dab60ed8d85d0ca560e9a2ad00a9
William Petit <william.petit@ptitcloud.fr>
2016-07-05 23:22:01 +0200

    Base plugin RSS

--- src/Controller/Widget/RSSWidgetController.php
+++ src/Controller/Widget/RSSWidgetController.php
@@ -4,16 +4,44 @@ namespace DashboardPlugin\Controller\Widget;
 
 use Karambol\Controller\Controller;
 use Karambol\KarambolApp;
+use PicoFeed\Reader\Reader;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RSSWidgetController extends Controller {
 
   public function mount(KarambolApp $app) {
     $app->get('/widgets/rss', [$this, 'showRSSWidget'])->bind('widget_rss');
+    $app->get('/widgets/rss/feed', [$this, 'fetchFeed'])->bind('widget_rss_feed');
   }
 
   public function showRSSWidget() {
     $twig = $this->get('twig');
     return $twig->render('widgets/rss/index.html.twig');
   }
+
+  public function fetchFeed() {
+
+    $request = $this->get('request');
+    $feedUrl = $request->query->get('u');
+
+    if(empty($feedUrl)) throw new BadRequestHttpException();
+
+    $reader = new Reader();
+    $resource = $reader->download($feedUrl);
+
+    $parser = $reader->getParser(
+        $resource->getUrl(),
+        $resource->getContent(),
+        $resource->getEncoding()
+    );
+
+    $feed = $parser->execute();
+
+    return new JsonResponse($feed);
+
+  }
+
+
 
 }
